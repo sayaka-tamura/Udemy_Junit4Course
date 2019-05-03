16029f0c848a7ece4c087ab56b19c6ec57c60fb0
sayaka-tamura <arccycle@gmail.com>
2019-05-03 20:59:26 +0900

    Initial Commit

---
+++ src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -0,0 +1,20 @@
+/**
+ * @author : Sayaka Tamura
+ * May 3, 2019
+ * Udemy Learn Java Unit Testing with JUnit 4
+ * https://github.com/in28minutes/JUnitIn28Minutes
+ */
+package com.in28minutes.junit.helper;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class StringHelperTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
