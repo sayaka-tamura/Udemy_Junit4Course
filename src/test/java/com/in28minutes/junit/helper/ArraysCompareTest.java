0e155d3075cf3014ebb69d1a4c197df5969d084f
sayaka-tamura <arccycle@gmail.com>
2019-05-04 11:31:35 +0900

    Add ArraysCompareTest class

---
+++ src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
@@ -0,0 +1,26 @@
+/**
+ * @author : Sayaka Tamura
+ * May 4, 2019
+ * Udemy Learn Java Unit Testing with JUnit 4 Section
+ * https://github.com/in28minutes/JUnitIn28Minutes
+ */
+package com.in28minutes.junit.helper;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class ArraysCompareTest {
+
+	// Arrays.sort
+
+	@Test
+	public void testArraySort_RandomArray() {
+		int[] numbers = { 12, 3, 4, 1 };
+		int[] expected = { 1, 3, 4, 12 };
+		Arrays.sort(numbers);
+		assertArrayEquals(expected, numbers);
+	}
+}
