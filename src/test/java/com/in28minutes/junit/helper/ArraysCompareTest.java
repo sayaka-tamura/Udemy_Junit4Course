1d73c788cde500fe9838c483be3581ee97dece26
sayaka-tamura <arccycle@gmail.com>
2019-05-04 13:17:41 +0900

    Add testSort_Performance test

--- src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
+++ src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
@@ -30,4 +30,13 @@ public class ArraysCompareTest {
 		Arrays.sort(numbers);
 	}
 
+	@Test(timeout = 100) // JUnit5: Check assertTimeout
+	public void testSort_Performance() {
+		int array[] = { 12, 23, 4 };
+		for (int i = 1; i < 1000000; i++) {
+			array[0] = i;
+			Arrays.sort(array);
+		}
+	}
+
 }
